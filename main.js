025f09806c06480c1ee26cf645f0eb33c34e0994
petrovsky747 <maxxxzv8@gmail.com>
2022-09-24 22:31:41 +0300

    The following functions have been added: palindrome (defining palindrome words), min/max (finding the smaller/greater of two received numbers), replaceZero (replacing 0 in array elements with zero)

--- main.js
+++ main.js
@@ -1,51 +1,51 @@
-let user = 'John Doe';
-console.log(user);
-console.log('----------------');
+(() => {
+  const palindrome = function (word) {
+    let result;
+    let letternum = 0;
 
-const student = 'Maxim';
-console.log(student);
-console.log('----------------');
+    for (let i = 0; i < word.length / 2; i++) {
+      letternum += 1;
+      result = word[i] === word[word.length - letternum];
+      if (result === false) {
+        break;
+      }
+    }
+    return result;
+  };
 
-user = student; // now the value "user" = "Мaxim"
-console.log(user);
-console.log('----------------');
+  const myword = 'шалаш';
+  console.log(palindrome(myword));
 
-let test = 1;
-test++;
-test += '1'; // now the value "test" = 21
-console.log(test);
-console.log('----------------');
+  const min = function (a, b) {
+    return a < b ? `${a}` : `${b}`;
+  };
 
-test -= 1; // now the value "test" = 20
-console.log(test);
-console.log('-----------------');
+  console.log(min(15, 5));
 
-test = !!test; //now the value "test" = false
-console.log(test);
-console.log('--------------');
+  const max = function (a, y) {
+    return a > y ? `${a}` : `${y}`;
+  };
 
-const mas = [2, 3, 5, 8];
+  console.log(max(15, 256));
 
-let result = 1;
+  const mas = [2, 50, 8, 100, 0, 6, 20, 3, 30, 58];
 
-for (let i = 0; i < mas.length; i++) {
-  result = result * mas[i];
-}
-console.log('произведение элементов массива равно ' + [result]);
-console.log('----------------');
+  const replaceZero = function (m) {
+    for (let i = 0; i < m.length; i++) {
+      m[i] = String(m[i]);
+      for (let j = 0; j < m[i].length; j++) {
+        if (m[i].indexOf('0') !== -1) {
+          m[i] = m[i].replace('0', 'zero');
+        }
+      }
+    }
+    for (let k = 0; k < m.length; k++) {
+      if (m[k].indexOf('zero') === -1) {
+        m[k] = Number(m[k]);
+      }
+    }
+    return m;
+  };
 
-const mas2 = [2, 5, 8, 15, 0, 6, 20, 3];
-
-for (let i = 0; i < mas2.length; i++) {
-  if ((mas2[i] > 5) & (mas2[i] < 10)) {
-    console.log('элемент массива, соответствующий условию равен ' + mas2[i]);
-  }
-}
-console.log('----------------');
-
-for (let i = 0; i < mas2.length; i++) {
-  const check = mas2[i] % 2;
-  if (check === 0) {
-    console.log('элемент массива, имеющий четное значение равен ' + mas2[i]);
-  }
-}
+  console.log(replaceZero(mas));
+})();
