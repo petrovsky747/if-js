b4cf5a4adaa88089a6840f9d7f7031e0d25d49fc
petrovsky747 <maxxxzv8@gmail.com>
2022-11-10 22:14:50 +0300

    Added changeStyle function, built on calling the next() iterator function. changeStyle changes the colors in order from the 'colors' object to the lines of text in the document.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,34 @@
-import Students from './entities/Students.js';
+const colors = {
+  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+  [Symbol.iterator]() {
+    return this;
+  },
+  next(color) {
+    this.current = this.data.indexOf(color);
+    if (this.current <= this.data.length - 2) {
+      this.current++;
+      color = this.data[this.current];
+      return {
+        done: false,
+        value: this.data[this.current],
+      };
+    } else {
+      color = this.data[0];
+      return {
+        value: this.data[0],
+      };
+    }
+  },
+};
+const changeStyle = (color) => (event) => {
+  event.target.style.color = colors.next(color).value;
+  color = event.target.style.color;
+};
 
-const studentsData = [
-  {
-    firstName: 'Василий',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Java',
-  },
-  {
-    firstName: 'Иван',
-    lastName: 'Иванов',
-    admissionYear: 2018,
-    courseName: 'JavaScript',
-  },
-  {
-    firstName: 'Александр',
-    lastName: 'Федоров',
-    admissionYear: 2017,
-    courseName: 'Python',
-  },
-  {
-    firstName: 'Николай',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Android',
-  },
-];
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
 
-const std1 = new Students(studentsData);
-console.log(std1.getInfo());
+text1.addEventListener('click', changeStyle());
+text2.addEventListener('click', changeStyle());
+text3.addEventListener('click', changeStyle());
