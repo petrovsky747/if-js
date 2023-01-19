b496de3d1e6332ebb131b455350a9c7e2a2b22b2
petrovsky747 <maxxxzv8@gmail.com>
2023-01-19 22:37:05 +0300

    Error adding slider.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -126,65 +126,208 @@ roomMinus.addEventListener('click', (event) => {
   roomFormText.textContent = `${roomAmount.toString()} Rooms`;
 });
 
-fetch('https://if-student-api.onrender.com/api/hotels/popular')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`${response.status} - ${response.statusText}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const addImages = () => {
-      const markupArr = [];
-      data.forEach((el, index) => {
-        const imgAlt = ['hotel', 'apartment', 'villa', 'hostel'];
-        markupArr[index] = `<div id="${el.id}" class="carousel-cell">
+const topsectionForm = document.querySelector('.topsection-form');
+
+const generateMarkup = (data) => {
+  const markupArr = [];
+  data.forEach((el, index) => {
+    markupArr[index] = `<div id="${el.id}" class="carousel-cell">
       <div>
-        <img class="homes-col__image" src="${el.imageUrl}" alt="${imgAlt[index]}">
+        <img class="homes-col__image" src="${el.imageUrl}" alt="Apartment photo">
         <p class="link homes-col__link">${el.name}</p>
           <p class="subtitle homes-col__subtitle">${el.city}, ${el.country}</p>
       </div>
     </div>`;
-      });
-      return markupArr.join('');
-    };
+  });
+  return markupArr.join('');
+};
 
-    const homesSection = `
+topsectionForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchKey = document.getElementById('city').value;
+  fetch(`https://if-student-api.onrender.com/api/hotels?search=${searchKey}`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application-json',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const availableSection = `
+<section class="available">
+  <div class="container available-container">
+    <h2 class="title available-title">Available hotels</h2>
+    <div class="main-carousel">
+      ${generateMarkup(data)}
+    </div>
+  </div>
+</section>
+`;
+      const currentSection = document.querySelector('.available');
+
+      if (currentSection !== null) {
+        currentSection.outerHTML = availableSection;
+      } else {
+        const advantagesSection = document.querySelector('.advantages');
+        advantagesSection.insertAdjacentHTML('beforebegin', availableSection);
+      }
+
+      const elem = document.querySelector('.main-carousel');
+      // eslint-disable-next-line no-undef
+      const flkty = new Flickity(elem, {
+        // options
+        cellAlign: 'center',
+        contain: true,
+        draggable: false,
+        groupCells: true,
+        pageDots: false,
+        percentPosition: false,
+        arrowShape: {
+          x0: 35,
+          x1: 60,
+          y1: 35,
+          x2: 70,
+          y2: 35,
+          x3: 45,
+        },
+      });
+      if (data.length <= 3) {
+        flkty.options.cellAlign = 'center';
+      }
+      flkty.next();
+      flkty.select(4);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+function addSlider() {
+  const elem = document.querySelector('.main-carousel');
+  // eslint-disable-next-line no-undef
+  const flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    draggable: false,
+    groupCells: true,
+    pageDots: false,
+    percentPosition: false,
+    arrowShape: {
+      x0: 35,
+      x1: 60,
+      y1: 35,
+      x2: 70,
+      y2: 35,
+      x3: 45,
+    },
+  });
+  flkty.next();
+  flkty.select(4);
+}
+
+const addData = (data) => {
+  const homesSection = `
 <section class="homes">
   <div class="container homes-container">
     <h2 class="title homes-title">Homes guests loves</h2>
     <div class="main-carousel">
-      ${addImages()}
+      ${generateMarkup(data)}
     </div>
   </div>
 </section>
 `;
 
-    const destinationsSection = document.querySelector('.destinations');
-    destinationsSection.insertAdjacentHTML('beforebegin', homesSection);
+  const destinationsSection = document.querySelector('.destinations');
+  destinationsSection.insertAdjacentHTML('beforebegin', homesSection);
 
-    const elem = document.querySelector('.main-carousel');
-    // eslint-disable-next-line no-undef
-    const flkty = new Flickity(elem, {
-      // options
-      cellAlign: 'left',
-      contain: true,
-      draggable: false,
-      groupCells: true,
-      pageDots: false,
-      percentPosition: false,
-      arrowShape: {
-        x0: 35,
-        x1: 60,
-        y1: 35,
-        x2: 70,
-        y2: 35,
-        x3: 45,
-      },
-    });
-    flkty.next();
-    flkty.select(4);
+  addSlider();
+};
+
+const dataKeys = JSON.parse(sessionStorage.getItem('data'));
+if (dataKeys !== null) {
+  console.log(dataKeys);
+  addData(dataKeys);
+} else {
+  fetch('https://if-student-api.onrender.com/api/hotels/popular', {
+    method: 'GET',
+    headers: {
+      'content-type': 'application-json',
+    },
   })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      sessionStorage.setItem('data', JSON.stringify(data));
+      console.log(data);
+      addData(data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+
+// fetch('https://if-student-api.onrender.com/api/hotels/popular', {
+//   method: 'GET',
+//   headers: {
+//     'content-type': 'application-json',
+//   },
+// })
+//   .then((response) => {
+//     debugger;
+//     if (!response.ok) {
+//       throw new Error(`${response.status} - ${response.statusText}`);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     debugger;
+//     sessionStorage.setItem('data', JSON.stringify(data));
+//     const homesSection = `
+// <section class="homes">
+//   <div class="container homes-container">
+//     <h2 class="title homes-title">Homes guests loves</h2>
+//     <div class="main-carousel">
+//       ${addContent(data)}
+//     </div>
+//   </div>
+// </section>
+// `;
+//
+//     const destinationsSection = document.querySelector('.destinations');
+//     destinationsSection.insertAdjacentHTML('beforebegin', homesSection);
+//
+//     const elem = document.querySelector('.main-carousel');
+//     // eslint-disable-next-line no-undef
+//     const flkty = new Flickity(elem, {
+//       // options
+//       cellAlign: 'left',
+//       contain: true,
+//       draggable: false,
+//       groupCells: true,
+//       pageDots: false,
+//       percentPosition: false,
+//       arrowShape: {
+//         x0: 35,
+//         x1: 60,
+//         y1: 35,
+//         x2: 70,
+//         y2: 35,
+//         x3: 45,
+//       },
+//     });
+//     flkty.next();
+//     flkty.select(4);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
